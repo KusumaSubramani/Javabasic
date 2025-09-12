f94ccc660a2b7f1bff47a0604815c29b414ba724
Library <Library@DESKTOP-T23CLJG>
2025-09-12 15:02:46 +0530

    interfaces

---
+++ Javabasic/src/interfaces/NonPrimeuser.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class NonPrimeuser implements Amazon {
+
+	@Override
+	public void addcharge() {
+		// TODO Auto-generated method stub
+		System.out.println("Service charge applied:"+serviceCharge);
+	}
+	
+	
+
+}
