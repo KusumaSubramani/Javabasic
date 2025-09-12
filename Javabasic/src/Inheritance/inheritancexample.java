4187e89b84df1d39a546f5d757d60894e14010ec
Library <Library@DESKTOP-T23CLJG>
2025-09-12 13:44:27 +0530

    updated

--- Javabasic/src/Inheritance/inheritancexample.java
+++ Javabasic/src/Inheritance/inheritancexample.java
@@ -5,7 +5,7 @@ public class inheritancexample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD");
-		System.out.println(" birthday");
+		System.out.println("happy birthday");
 	}
 
 }
