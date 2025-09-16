3053cde9330633f88eae96e0fc6a988113d6836d
Library <Library@DESKTOP-T23CLJG>
2025-09-16 14:19:30 +0530

    Code with Arithematic Exception error

--- Javabasic/src/Exception_handeling/Demo.java
+++ Javabasic/src/Exception_handeling/Demo.java
@@ -4,13 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		int d=0;
-		try {
 		int a=42/d;
-		}
-		catch(ArithmeticException e)
-		{
-			System.out.println("exception handeled" +e.getMessage());
-		}
 		System.out.println("will not be printed");
 
 	}
