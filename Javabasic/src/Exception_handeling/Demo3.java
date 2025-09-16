3ad10957f5882f0103b8498581f865e327394634
Library <Library@DESKTOP-T23CLJG>
2025-09-16 14:29:13 +0530

    Checking by adding multiple catch blocks

---
+++ Javabasic/src/Exception_handeling/Demo3.java
@@ -0,0 +1,22 @@
+package Exception_handeling;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int d=0;
+		try {
+		int a=42/d;
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("exception handeled" +e.getMessage());
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("exception handeled" +e.getMessage());
+		}
+		System.out.println("will not be printed");
+
+	}
+
+}
