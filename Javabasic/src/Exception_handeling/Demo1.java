a9836b52f043bb4bfa884407daf3136ef810bbea
Library <Library@DESKTOP-T23CLJG>
2025-09-16 14:20:59 +0530

    Exception handeled with try and catch keyword

---
+++ Javabasic/src/Exception_handeling/Demo1.java
@@ -0,0 +1,19 @@
+
+package Exception_handeling;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int d=0;
+		try {
+		int a=42/d;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("exception handeled" +e.getMessage());
+		}
+		System.out.println("will not be printed");
+
+	}
+
+}
