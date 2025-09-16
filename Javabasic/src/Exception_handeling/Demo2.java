d10420eb89a7d89bcca4ee3edf8084d0f3f78fe5
Library <Library@DESKTOP-T23CLJG>
2025-09-16 14:25:03 +0530

    Exception handeling using finally keyword

---
+++ Javabasic/src/Exception_handeling/Demo2.java
@@ -0,0 +1,21 @@
+package Exception_handeling;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int d=0;
+		try {
+		int a=42/d;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("exception handeled" +e.getMessage());
+		}
+		finally {
+			System.out.println("it will always execute");
+		}
+		System.out.println("will not be printed");
+
+	}
+
+}
