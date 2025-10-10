5021e230ec35f0c24a27b871e58883ebd5d4890b
Library <Library@DESKTOP-T23CLJG>
2025-10-10 13:03:29 +0530

    generic

---
+++ Javabasic/src/Generics/Exec.java
@@ -0,0 +1,13 @@
+package Generics;
+
+public class Exec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Generic_program <String> obj=new Generic_program();
+		obj.setData("hello");
+		System.out.println("the string obj is"+obj.getData());
+
+	}
+
+}
---
+++ Javabasic/src/Generics/Generic_program.java
@@ -0,0 +1,15 @@
+package Generics;
+
+public class Generic_program <T>{ 
+	T data;
+
+	public T getData() {
+		return data;
+	
+
+	}
+	public void setData(T data) {
+		this.data=data;
+	}
+
+}
